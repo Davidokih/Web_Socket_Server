0536885421c245bfd5a20e8c538c3a50aa6f3dfb
Davidokih <davidyole023@gmail.com>
2023-08-28 14:21:55 +0100

    update

---
+++ Middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config;
+
+module.exports = async (req, res) => {
+    try {
+        if (!req.headers.authorization) throw "Forbidden";
+        const token = req.headers.authorization.split(" ")[ 1 ];
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.payload = payload;
+
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Forbidden" });
+    }
+};
